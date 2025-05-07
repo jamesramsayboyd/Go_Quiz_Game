291c5a0f8d4bfc6e504a83f712c19cdf7cdf189e
jamesramsayboyd <jamesramsayboyd@gmail.com>
2025-05-07 14:47:46 +0800

    Simplified things

--- quiz.go
+++ quiz.go
@@ -10,43 +10,46 @@ import (
 	"time"
 )
 
+// Struct representing a question. Standard OOP class is not present in Go
 type question struct{
-	ques string
-	ans string
+	question string
+	answer string
 }
 
-func generateQuestions(fileName string, shuffle bool) []question{
+
+func loadQuestions(fileName string) []question{
 
 	file, err := os.Open(fileName)
 	if err != nil {
-		fmt.Println("Error :- The CSV file could not be read.");
-		panic(err)
+		fmt.Println("Error: File not found.")
+		//panic(err)
 	}
-	defer file.Close();
+	//defer file.Close();
 
-	quesAns, err := csv.NewReader(file).ReadAll()
+	csvContents, err := csv.NewReader(file).ReadAll()
 	if err != nil {
-		fmt.Println("Error :- The questions from the CSV file could not be read.");
-		panic(err)
+		fmt.Println("Error: Could not read questions.")
+		//panic(err)
 	}
 
 	var quiz []question
 
 
-	for _, line := range quesAns {
+	for _, line := range csvContents {
 		qa := question{
-			ques: line[0],
-			ans: line[1],
+			question: line[0],
+			answer: line[1],
 		}
 		quiz = append(quiz, qa)
 	}
-	if shuffle {
-		r := rand.New(rand.NewSource(time.Now().Unix()))
+	
+	// Random number generation to shuffle question order
+	r := rand.New(rand.NewSource(time.Now().Unix()))
 		for n := len(quiz); n > 0; n-- {
 			randIndex := r.Intn(n)
 			quiz[n-1], quiz[randIndex] = quiz[randIndex], quiz[n-1]
 		}
-	}
+	
 	return quiz
 }
 
@@ -54,19 +57,18 @@ func generateQuestions(fileName string, shuffle bool) []question{
 
 func main(){
 
-	fmt.Println("Welcome to the CSV Quiz Game designed in Go Lang");
+	//fmt.Println("Welcome to the CSV Quiz Game designed in Go Lang");
 	fileName := flag.String("questions","problems.csv","The CSV file from where questions would be read.")
 	timeLimit := flag.Int("time", 30, "Set a time limit for the quiz")
-	shuffle := flag.Bool("shuffle",false,"Shuffle the order of questions")
 	flag.Parse()
 
 	var quiz []question
 	var score int
 	score = 0
-	quiz = generateQuestions(*fileName, *shuffle)
+	quiz = loadQuestions(*fileName)
 	var answer string
 
-	fmt.Printf("Press Enter key to start the quiz. Duration of quiz :- %d seconds\n",*timeLimit)
+	fmt.Printf("Press Enter to start the quiz")
 	fmt.Scanln()
 	timer := time.NewTimer(time.Duration(*timeLimit)*time.Second)
 	defer timer.Stop()
@@ -78,9 +80,9 @@ func main(){
 		}()
 
 	for i, questions := range quiz {
-		fmt.Printf("Question %d :- %s? ",i+1,questions.ques)
+		fmt.Printf("Question %d :- %s? ",i+1,questions.question)
 		fmt.Scan(&answer)
-		if strings.ToLower(strings.Trim(answer," ")) == strings.ToLower(strings.Trim(questions.ans, " ")){
+		if strings.ToLower(strings.Trim(answer," ")) == strings.ToLower(strings.Trim(questions.answer, " ")){
 			score++;
 		}
 	}
